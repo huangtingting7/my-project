bb4aa7908c5a097b50904d4a1fdc28ea0b0eb0e3
huangtingting <214604144@qq.com>
2019-12-02 17:51:44 +0800

    添加统计图

--- src/components/homepage/map.vue
+++ src/components/homepage/map.vue
@@ -7,7 +7,31 @@
     </div>
     <div class="content">
       <!-- 首页左侧 -->
-      <div class="leftcontent"></div>
+      <div class="leftcontent">
+        <div class="headList">
+          <ul>
+          <li class="title1">
+            <div class="with15">区域</div>
+            <div class="with10">机床总数</div>
+            <div class="with25">机床实时状态及数量</div>
+          </li>
+          <li v-for="(item,index) in headlist" :key="index">
+            <div class="with15">{{item.area}}</div>
+            <div class="with10">{{item.total}}台</div>
+            <div class="with25">
+              <template>
+                <Tooltip content="3 done / 3 in progress / 4 to do" style="width: 100%">
+                    <Progress :percent="item.firstNum" :success-percent="item.secondNum" />
+                </Tooltip>
+            </template>
+            </div>
+          </li>
+        </ul>
+        </div>
+        <div class="headBottom">
+          <div id="myChartBottom" :style="{width: '100%', height: '500px'}"></div>
+        </div>
+      </div>
       <div class="map">
         <!-- 中国地图 -->
         <div id="myChartChina" :style="{width: '100%', height: '500px'}"
@@ -35,7 +59,42 @@
       </div>
       <!-- 首页右侧 -->
       <div class="echartinfo">
-        
+        <div id="myChartRight" :style="{width: '100%', height: '500px'}"></div>
+        <div class="infolist">
+          <ul>
+          <li class="title">
+            <div>加工</div>
+            <div>故障</div>
+            <div>修机</div>
+            <div>未连接</div>
+          </li>
+          <li>
+            <div>120</div>
+            <div>120</div>
+            <div>46</div>
+            <div>192</div>
+          </li>
+        </ul>
+        </div>
+        <div id="myChartRightBottom" :style="{width: '100%', height: '500px'}"></div>
+        <div class="infolist">
+          <ul>
+          <li class="title">
+            <div>龙门移动式</div>
+            <div>卧式加工中心</div>
+            <div>立式加工中心</div>
+            <div>横梁移动式</div>
+            <div>高速型材/复材加工中心</div>
+          </li>
+          <li>
+            <div>144</div>
+            <div>48</div>
+            <div>149</div>
+            <div>72</div>
+            <div>74</div>
+          </li>
+        </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -46,6 +105,14 @@ import "echarts/map/js/china.js";
 export default {
   data() {
     return {
+      headlist: [
+        {id:1, area: '西南办事处', total: 100, firstNum: 20, secondNum: 30, thridNum: 10},
+        {id:1, area: '西北办事处', total: 70, firstNum: 40, secondNum: 20, thridNum: 15},
+        {id:1, area: '华东办事处', total: 80, firstNum: 50, secondNum: 10, thridNum: 10},
+        {id:1, area: '华北办事处', total: 90, firstNum: 30, secondNum: 15, thridNum: 20},
+        {id:1, area: '东北办事处', total: 80, firstNum: 30, secondNum: 15, thridNum: 20},
+        {id:1, area: '华南办事处', total: 60, firstNum: 30, secondNum: 15, thridNum: 20},
+      ],
       infolist: [
         { index:1,name: "客户1", jcxh: "VCER5512", warningtime: "2019.09.20", xlh: "12638055",warncode:"7rg6566"},
         { index:2,name: "客户2", jcxh: "VC325", warningtime: "2019.09.20", xlh: "12638055",warncode:"7rg6566" },
@@ -57,6 +124,9 @@ export default {
   },
   mounted() {
     this.drawLine();
+    this.getBottom();
+    this.getRight();
+    this.getRightBottom();
   },
   methods: {
     drawLine() {
@@ -161,6 +231,160 @@ export default {
         myChartChina.resize();
       };
     },
+    getBottom () {
+      // 基于准备好的dom，初始化echarts实例
+      // 数据组织
+      let titleList = []
+      let seriesList = []
+      if (this.headlist && this.headlist.length > 0) {
+        this.headlist.forEach((n) => {
+          titleList.push(n.area)
+          seriesList.push(n.total)
+        })
+      }
+      let myChart = this.$echarts.init(document.getElementById('myChartBottom'))
+      var option = {
+        title: {
+          text: '单位:台',
+          textStyle: {
+            fontWeight: 'normal',              //标题颜色
+            color: '#fff',
+            fontSize: 12,
+            marginTop: 30,
+          },
+        },
+        color: ['#3398DB'],
+        tooltip : {
+          trigger: 'axis',
+          axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+              type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+          },
+          textStyle: {
+            color: '#fff'
+          }
+        },
+        grid: {
+          lineStyle: {
+              color: '#fff'
+          },
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis : [
+          {
+              type : 'category',
+              data : titleList,
+              axisTick: {
+                  alignWithLabel: true
+              }
+          },
+        ],
+        yAxis : [
+          {
+              type : 'value'
+          }
+        ],
+        series : [
+          {
+            name:'直接访问',
+            type:'bar',
+            barWidth: '50%',
+            data: seriesList,
+          }
+        ]
+      }
+			// var myChart = echarts.init(document.getElementById('myChartBottom'))
+			// // 使用刚指定的配置项和数据显示图表。
+			myChart.setOption(option)
+    },
+    getRight () {
+      let option = {
+          title: {
+              text: '全国机床状态实时分布',
+              left: 'center'
+          },
+          tooltip : {
+              trigger: 'item',
+              formatter: "{a} <br/>{b} : {c} ({d}%)"
+          },
+          legend: {
+              bottom: 10,
+              left: 'center',
+              data: ['加工', '故障','修机','未连接']
+          },
+          series : [
+              {
+                  type: 'pie',
+                  radius: ['42%', '53%'], // 半径
+                  center: ['50%', '50%'],
+                  selectedMode: 'single',
+                  data:[
+                      {value:120,name: '加工'},
+                      {value:120, name: '故障'},
+                      {value:46, name: '修机'},
+                      {value:192, name: '未连接'},
+                  ],
+                  itemStyle: {
+                      emphasis: {
+                          shadowBlur: 10,
+                          shadowOffsetX: 0,
+                          shadowColor: 'rgba(0, 0, 0, 0.5)'
+                      }
+                  }
+              }
+          ]
+      };
+
+      let myChart = this.$echarts.init(document.getElementById('myChartRight'))
+      // // 使用刚指定的配置项和数据显示图表。
+			myChart.setOption(option)
+    },
+    getRightBottom () {
+      console.log('2')
+      let option = {
+          title: {
+              text: '全国机床产品系列分布',
+              left: 'center'
+          },
+          tooltip : {
+              trigger: 'item',
+              formatter: "{a} <br/>{b} : {c} ({d}%)"
+          },
+          legend: {
+              bottom: 10,
+              left: 'center',
+              data: ['龙门移动式', '卧式加工中心','立式加工中心','横梁移动式', '高速型材/复材加工中心']
+          },
+          series : [
+              {
+                  type: 'pie',
+                  radius: ['42%', '53%'], // 半径
+                  center: ['50%', '50%'],
+                  selectedMode: 'single',
+                  data:[
+                      {value:140,name: '龙门移动式'},
+                      {value:48, name: '卧式加工中心'},
+                      {value:149, name: '立式加工中心'},
+                      {value:72, name: '横梁移动式'},
+                      {value:72, name: '高速型材/复材加工中心'},
+                  ],
+                  itemStyle: {
+                      emphasis: {
+                          shadowBlur: 10,
+                          shadowOffsetX: 0,
+                          shadowColor: 'rgba(0, 0, 0, 0.5)'
+                      }
+                  }
+              }
+          ]
+      };
+
+      let myChart = this.$echarts.init(document.getElementById('myChartRightBottom'))
+      // // 使用刚指定的配置项和数据显示图表。
+			myChart.setOption(option)
+    },
     back(){
       this.$router.push("index")
     }
@@ -174,7 +398,8 @@ h2 {
 }
 .wrapper {
   width: 100%;
-  height: 100%;
+  height:calc(100% - 100px);
+  /* height: 100%; */
   display: flex;
   flex-direction: column;
   background: -webkit-linear-gradient(45deg, #263e7d, #278bc7, #263e7d);
@@ -321,5 +546,44 @@ h2 {
 }
 .warning {
   margin-right: 2em;
+}
+.headList{
+  margin-top: 4rem;
+  width: 100%;
+  height: auto;
+}
+.headList > ul {
+  width: 86%;
+  height: auto;
+  margin: 0 auto;
+}
+.headList > ul > li {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1em;
+  border-bottom: 1px solid #1c92b6;
+}
+/* .headList > ul > li > div {
+  width: 25%;
+  text-align: center;
+} */
+.with10 {
+  width: 15%;
+  text-align: center;
+}
+.with15 {
+  width: 15%;
+  text-align: left;
+}
+.with25 {
+  width: 65%;
+  text-align: left;
+}
+.title1 {
+  border: 0;
+  font-size: 16px;
+  font-weight: bold;
 }
 </style>
